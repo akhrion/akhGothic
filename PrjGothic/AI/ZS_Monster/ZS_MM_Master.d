5f9e41147584208802d3ce16f222cfce521e6960
Maxim <74652627+akhrion@users.noreply.github.com>
2022-04-28 23:15:27 +0300

    DM

--- PrjGothic/AI/ZS_Monster/ZS_MM_Master.d
+++ PrjGothic/AI/ZS_Monster/ZS_MM_Master.d
@@ -18,414 +18,7 @@ func int isDmgOnlyPierce()
 	};
 	return true;
 };
-//Одноимённые функции ниже, запоминают и возвращают
-//количество полученного целью урона, в процентах от макс. хп
-//Хранимые значения: от 0% до 99%
-//Т.е. мы можем узнать сколько урона и какого типа, ~получил таргет.
-//Используется в функции выдачи трофеев.
-func int C_Npc_GetDamageRecieved(var C_NPC npc, var int dmgInxType)
-{
-	if(Hlp_IsValidNpc(npc))
-	{
-		if(dmgInxType == DAM_INDEX_EDGE)
-		{
-			return npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,6) % expon(10,2);
-		}
-		else if(dmgInxType == DAM_INDEX_FIRE)
-		{
-			return npc.aivar[AIV_MM_DamageRecieved_Hash] / 10000 % 100;
-//			return npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,4) % expon(10,2);
-		}
-		else if(dmgInxType == DAM_INDEX_MAGIC)
-		{
-			return npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,2) % expon(10,2);
-		}
-		else if(dmgInxType == DAM_INDEX_POINT)
-		{
-			return npc.aivar[AIV_MM_DamageRecieved_Hash] % expon(10,2);
-		}
-		else
-		{
-			return 100;
-		};
-	};
-	return 101;
-};
-func int C_Npc_GetDamageRecieved_War(var C_NPC npc)
-{
-	return (npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,6)) % expon(10,2);
-};
-func int C_Npc_GetDamageRecieved_Fire(var C_NPC npc)
-{
-	return (npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,4)) % expon(10,2);
-};
-func int C_Npc_GetDamageRecieved_Magic(var C_NPC npc)
-{
-	return (npc.aivar[AIV_MM_DamageRecieved_Hash] / expon(10,2)) % expon(10,2);
-};
-func int C_Npc_GetDamageRecieved_Hunt(var C_NPC npc)
-{
-	return npc.aivar[AIV_MM_DamageRecieved_Hash] % expon(10,2);
-};
-func int C_Npc_GDR_AvailablePrcnt(var C_NPC npc)
-{
-	return 99
-	- (
-		  C_Npc_GetDamageRecieved(npc,DAM_INDEX_EDGE)
-		+ C_Npc_GetDamageRecieved(npc,DAM_INDEX_FIRE)
-		+ C_Npc_GetDamageRecieved(npc,DAM_INDEX_MAGIC)
-		+ C_Npc_GetDamageRecieved(npc,DAM_INDEX_POINT)
-	);
-};
-func void C_Npc_SetDamageRecieved(var C_NPC npc,var int dmg,var int dmgInxType)
-{
-	if(dmg)
-	{
-		var int prcnt; var int curDmg; var int aPP; var int aPC;
-		prcnt = dmg * 100 / npc.attribute[ATR_HITPOINTS_MAX];
-		aPP = C_Npc_GDR_AvailablePrcnt(npc);
-		aPC = 99 - C_Npc_GetDamageRecieved(npc,dmgInxType);
-		curDmg =	tern(prcnt > aPP, aPP,
-					tern(prcnt > aPC, aPC,prcnt)
-		);
-		if(dmgInxType == DAM_INDEX_EDGE)
-		{
-			npc.aivar[AIV_MM_DamageRecieved_Hash] += curDmg * expon(10,6);
-		}
-		else if(dmgInxType == DAM_INDEX_FIRE)
-		{
-			npc.aivar[AIV_MM_DamageRecieved_Hash] += curDmg * expon(10,4);
-		}
-		else if(dmgInxType == DAM_INDEX_MAGIC)
-		{
-			npc.aivar[AIV_MM_DamageRecieved_Hash] += curDmg * expon(10,2);
-		}
-		else if(dmgInxType == DAM_INDEX_POINT)
-		{
-			npc.aivar[AIV_MM_DamageRecieved_Hash] += curDmg;
-		};
-	};
-};
-func void C_Npc_SetDamageRecieved_War(var C_NPC npc,var int dmg)
-{
-	if(dmg)
-	{
-		var int prcnt; var int curDmg;
-		prcnt = dmg * 100 / npc.attribute[ATR_HITPOINTS_MAX];
-		curDmg = C_Npc_GetDamageRecieved_War(npc);
-		curDmg = tern(curDmg + prcnt > 99, 99, curDmg + prcnt);
-		npc.aivar[AIV_MM_DamageRecieved_Hash] = curDmg * expon(10,6);
-	};
-};
-func void C_Npc_SetDamageRecieved_Fire(var C_NPC npc,var int dmg)
-{
-	if(dmg)
-	{
-		var int prcnt; var int curDmg;
-		prcnt = dmg * 100 / npc.attribute[ATR_HITPOINTS_MAX];
-		curDmg = C_Npc_GetDamageRecieved_Fire(npc);
-		curDmg = tern(curDmg + prcnt > 99, 99, curDmg + prcnt);
-		npc.aivar[AIV_MM_DamageRecieved_Hash] = curDmg * expon(10,4);
-	};
-};
-func void C_Npc_SetDamageRecieved_Magic(var C_NPC npc,var int dmg)
-{
-	if(dmg)
-	{
-		var int prcnt; var int curDmg;
-		prcnt = dmg * 100 / npc.attribute[ATR_HITPOINTS_MAX];
-		curDmg = C_Npc_GetDamageRecieved_Magic(npc);
-		curDmg = tern(curDmg + prcnt > 99, 99, curDmg + prcnt);
-		npc.aivar[AIV_MM_DamageRecieved_Hash] = curDmg * expon(10,2);
-	};
-};
-func void C_Npc_SetDamageRecieved_Hunt(var C_NPC npc,var int dmg)
-{
-	if(dmg)
-	{
-		var int prcnt; var int curDmg;
-		prcnt = dmg * 100 / npc.attribute[ATR_HITPOINTS_MAX];
-		curDmg = C_Npc_GetDamageRecieved_Hunt(npc);
-		curDmg = tern(curDmg + prcnt > 99, 99, curDmg + prcnt);
-		npc.aivar[AIV_MM_DamageRecieved_Hash] = curDmg;
-	};
-};
-func int Spell_HasFireDamage(var int splInx)
-{
-	if(
-		splInx == SPL_FIREBALL
-	||	splInx == SPL_FIREBOLT
-	||	splInx == SPL_FIRESTORM
-	||	splInx == SPL_FIRERAIN
-	||	splInx == SPL_PYROKINESIS
-	)
-	{
-		return true;
-	};
-	return false;
-};
-func int Spell_HasMagicDamage(var int splInx)
-{
-	if(
-		splInx == SPL_CHAINLIGHTNING
-	||	splInx == SPL_ICECUBE
-	||	splInx == SPL_ICEWAVE
-	||	splInx == SPL_THUNDERBALL
-	||	splInx == SPL_THUNDERBOLT
-	||	splInx == SPL_STORMFIST
-	||	splInx == SPL_WINDFIST
-	||	splInx == SPL_BREATHOFDEATH
-	||	splInx == SPL_MASSDEATH
-	||	splInx == SPL_DESTROYUNDEAD
-	)
-	{
-		return true;
-	};
-	return false;
-};
 
-func int Spell_GetDamage(var int spellInx)
-{
-	var int dmg;
-	if(spellInx == SPL_FIREBOLT){dmg = SPL_DAMAGE_FIREBOLT;};
-	if(spellInx == SPL_FIREBALL){dmg = SPL_DAMAGE_FIREBALL;};
-	if(spellInx == SPL_FIRESTORM){dmg = SPL_DAMAGE_FIRESTORM;};
-	if(spellInx == SPL_FIRERAIN){dmg = SPL_DAMAGE_FIRERAIN;};
-	if(spellInx == SPL_THUNDERBOLT){dmg = SPL_DAMAGE_THUNDERBOLT;};
-	if(spellInx == SPL_THUNDERBALL){dmg = SPL_DAMAGE_THUNDERBALL;};
-	if(spellInx == SPL_MASSDEATH){dmg = SPL_DAMAGE_MASSDEATH;};
-	if(spellInx == SPL_BREATHOFDEATH){dmg = SPL_DAMAGE_BREATHOFDEATH;};
-	if(spellInx == SPL_DESTROYUNDEAD){dmg = SPL_DAMAGE_DESTROYUNDEAD;};
-	if(spellInx == SPL_WINDFIST){dmg = SPL_DAMAGE_WINDFIST;};
-	if(spellInx == SPL_STORMFIST){dmg = SPL_DAMAGE_STORMFIST;};
-	if(spellInx == SPL_ICECUBE){dmg = SPL_FREEZE_DAMAGE;};
-	if(spellInx == SPL_ICEWAVE){dmg = SPL_FREEZE_DAMAGE;};
-	if(spellInx == SPL_CHAINLIGHTNING){dmg = SPL_ZAPPED_DAMAGE_PER_SEC;};
-	if(spellInx == SPL_PYROKINESIS){dmg = SPL_PYRO_DAMAGE_PER_SEC;};
-	return dmg;
-};
-
-func int Npc_GetLastHitSpellLevel(var C_NPC npc)
-{
-	return npc.aivar[AIV_MM_DamageRecieved_Hash] / 100000000 % 100;
-};
-func void Npc_SetLastHitSpellLevel(var C_NPC npc, var int spellLevel)
-{
-	npc.aivar[AIV_MM_DamageRecieved_Hash] = spellLevel * 100000000;
-};
-func int Npc_IsDissected(var C_NPC npc)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT_DISSECTED))
-	{
-		return true;
-	};
-	return false;
-};
-func int Npc_IsFired(var C_NPC npc)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT_FIRED))
-	{
-		return true;
-	};
-	return false;
-};
-func int Npc_IsBurned(var C_NPC npc)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT_BURNED))
-	{
-		return true;
-	};
-	return false;
-};
-func int Npc_IsVerkohlt(var C_NPC npc)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT_VERKOHLT))
-	{
-		return true;
-	};
-	return false;
-};
-func int Npc_IsAsh(var C_NPC npc)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT_ASH))
-	{
-		return true;
-	};
-	return false;
-};
-func void C_Npc_RemoveVisualTypeFlag(var C_NPC npc, var int VT)
-{
-	if(isFlagsContainCategorie(npc.aivar[AIV_MM_VisualType], VT))
-	{
-		npc.aivar[AIV_MM_VisualType] -= VT;
-	};
-};
-func void C_Npc_SetVisualTypeFlag(var C_NPC npc, var int VT)
-{
-	npc.aivar[AIV_MM_VisualType] += VT;
-};
-func void SetVisualType(var C_NPC npc)
-{
-	//akh working
-	//refact
-	var int fireDmg;
-	fireDmg = C_Npc_GetDamageRecieved(npc,DAM_INDEX_FIRE);
-	if(
-		fireDmg > 80
-	&&	!Npc_IsAsh(npc)
-	)
-	{
-		C_Npc_SetVisualTypeFlag(npc,VT_ASH);
-	}
-	else if(
-		fireDmg > 60
-	&&	!Npc_IsVerkohlt(npc)
-	)
-	{
-		C_Npc_SetVisualTypeFlag(npc,VT_VERKOHLT);
-	}
-	else if(
-		fireDmg > 40
-	&&	!Npc_IsBurned(npc)
-	)
-	{
-		C_Npc_SetVisualTypeFlag(npc,VT_BURNED);
-	}
-	else if(
-		fireDmg > 20
-	&&	!Npc_IsFired(npc)
-	)
-	{
-		C_Npc_SetVisualTypeFlag(npc,VT_FIRED);
-	};
-	var int warDmg;
-	warDmg = C_Npc_GetDamageRecieved(npc,DAM_INDEX_EDGE);
-	if(
-		warDmg > 20
-	&&	!Npc_IsDissected(npc)
-	)
-	{
-		C_Npc_SetVisualTypeFlag(npc,VT_DISSECTED);
-	};
-};
-func int SRD_EffectiveDamageTypes_Count(var int wd_,var int fd_, var int md_,var int hd_)
-{
-	var int c; c = 0;
-	if(wd_){c +=1;};
-	if(fd_){c +=1;};
-	if(md_){c +=1;};
-	if(hd_){c +=1;};
-	return c;
-};
-func void SaveReceivedDamage_Monster()
-{
-	//Нужно имплиментировать детекцию крита
-	//
-	var c_ITEM itm;
-	var int WarDmg; var int FireDmg; var int HuntDmg;  var int MgcDmg;
-	var int OthStr;
-	WarDmg = 0; FireDmg = 0; HuntDmg = 0; MgcDmg = 0; OthStr = 0;
-	if(Npc_IsInFightMode(other,FMODE_MAGIC))
-	{
-		if(Spell_HasFireDamage(Npc_GetActiveSpell(other)))
-		{
-			FireDmg += self.aivar[AIV_MM_LASTHPSTATE] - self.attribute[ATR_HITPOINTS];
-		};
-		if(Spell_HasMagicDamage(Npc_GetActiveSpell(other)))
-		{
-			MgcDmg += Spell_GetDamage(Npc_GetActiveSpell(other)) * Npc_GetLastHitSpellLevel(self) - self.protection[PROT_MAGIC];
-		};
-	}
-	else if(Npc_HasReadiedWeapon(other))
-	{
-		itm = Npc_GetReadiedWeapon(other);
-		MgcDmg += tern(
-			(itm.damage[DAM_INDEX_BARRIER] - self.protection[PROT_BARRIER]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_BARRIER] - self.protection[PROT_BARRIER])
-		);
-		if(Npc_IsInFightMode(other,FMODE_MELEE))
-		{
-			if(
-				isFlagsContainCategorie(itm.damagetype,DAM_BLUNT)
-			)
-			{
-				if(
-					isFlagsContainCategorie(itm.damagetype,DAM_EDGE)
-				)
-				{
-					othStr = other.attribute[ATR_STRENGTH] / 2;
-					WarDmg += tern(
-						((itm.damage[DAM_INDEX_BLUNT] + othStr) - self.protection[PROT_BLUNT]) < 0,
-						0,
-						((itm.damage[DAM_INDEX_BLUNT] + othStr) - self.protection[PROT_BLUNT])
-					);
-					WarDmg += tern(
-						((itm.damage[DAM_INDEX_EDGE] + othStr) - self.protection[PROT_EDGE]) < 0,
-						0,
-						((itm.damage[DAM_INDEX_EDGE] + othStr) - self.protection[PROT_EDGE])
-					);
-				}
-				else
-				{
-					othStr = other.attribute[ATR_STRENGTH];
-					WarDmg += tern(
-						((itm.damage[DAM_INDEX_BLUNT] + othStr) - self.protection[PROT_BLUNT]) < 0,
-						0,
-						((itm.damage[DAM_INDEX_BLUNT] + othStr) - self.protection[PROT_BLUNT])
-					);
-				};
-			}
-			else if(isFlagsContainCategorie(itm.damagetype,DAM_EDGE))
-			{
-				othStr  = other.attribute[ATR_STRENGTH];
-				WarDmg += tern(
-					((itm.damage[DAM_INDEX_EDGE] + othStr) - self.protection[PROT_EDGE]) < 0,
-					0,
-					((itm.damage[DAM_INDEX_EDGE] + othStr) - self.protection[PROT_EDGE])
-				);
-			};
-		};
-		FireDmg += tern(
-			(itm.damage[DAM_INDEX_FIRE] - self.protection[PROT_FIRE]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_FIRE] - self.protection[PROT_FIRE])
-		);
-		WarDmg += tern(
-			(itm.damage[DAM_INDEX_FLY] - self.protection[PROT_FLY]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_FLY] - self.protection[PROT_FLY])
-		);
-		MgcDmg += tern(
-			(itm.damage[DAM_INDEX_MAGIC] - self.protection[PROT_MAGIC]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_MAGIC] - self.protection[PROT_MAGIC])
-		);
-		HuntDmg += tern(
-			(itm.damage[DAM_INDEX_POINT] - self.protection[PROT_POINT]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_POINT] - self.protection[PROT_POINT])
-		);
-		HuntDmg += tern(
-			(itm.damage[DAM_INDEX_FALL] - self.protection[PROT_FALL]) < 0,
-			0,
-			(itm.damage[DAM_INDEX_FALL] - self.protection[PROT_FALL])
-		);
-	}
-	else if(Npc_IsInFightMode(other,FMODE_FIST))
-	{
-		WarDmg += tern(
-			(other.attribute[ATR_STRENGTH] - self.protection[PROT_BLUNT]) < 0,
-			0,
-			(other.attribute[ATR_STRENGTH] - self.protection[PROT_BLUNT])
-		);
-	};
-	C_Npc_SetDamageRecieved(self,WarDmg,DAM_INDEX_EDGE);
-	C_Npc_SetDamageRecieved(self,FireDmg,DAM_INDEX_FIRE);
-	C_Npc_SetDamageRecieved(self,MgcDmg,DAM_INDEX_MAGIC);
-	C_Npc_SetDamageRecieved(self,HuntDmg,DAM_INDEX_POINT);
-	self.aivar[AIV_MM_LASTHPSTATE] = self.attribute[ATR_HITPOINTS];
-};
 
 func void SaveReceivedDamage_Monster_Old()
 {
@@ -576,6 +169,7 @@ func void akh_DebugSenseSystem()
 };
 func void B_MM_AssessEnemy()
 {
+//	Print("B_MM_AssessEnemy");
 	MinecrawlerOM(self,other);
 	PrintDebugNpc(PD_MST_FRAME,"B_MM_AssessEnemy");
 //	PrintI(Npc_GetRange_See(self));
@@ -728,6 +322,8 @@ func void B_MM_ReactToDamage()
 {
 	PrintDebugNpc(PD_MST_FRAME,"B_MM_ReactToDamage");
 	PrintGlobals(PD_MST_CHECK);
+	SC_OneShot(other,self);
+	SC_RescaleDamage();
 	SaveReceivedDamage_Monster();
 	SetVisualType(self);
 	self.aivar[AIV_PLUNDERED] = PRIO_ATTACKER;
@@ -741,11 +337,10 @@ func void B_MM_ReactToDamage()
 	}
 	else if(C_NpcIsDangerousPerson(self,other))
 	{
-		
 	}
 	else
 	{
-		PrintDebugNpc(PD_MST_FRAME,"...Monster ist JВger");
+		PrintDebugNpc(PD_MST_FRAME,"...Monster ist JР вЂ™РїС—Р…ger");
 		Npc_SetTarget(self,other);
 		Npc_ClearAIQueue(self);
 		Npc_PercDisable(self,PERC_ASSESSENEMY);
@@ -777,18 +372,35 @@ func void B_MM_ReactToOthersDamage()
 		AI_StartState(self,ZS_MM_Attack,0,"");
 	};
 };
-
+var C_NPC B_NPC_LASTTARGET;
+func void B_Npc_ChoiceTarget(var C_NPC npc)
+{
+	B_NPC_LASTTARGET = Hlp_GetNpc(self.aivar[AIV_LASTTARGET]);
+	if(Npc_GetDistToNpc(self,B_NPC_LASTTARGET) > Npc_GetDistToNpc(self,other))
+	{
+		Npc_SetTarget(self,other);
+		B_Npc_SetLastTargetByID(self,Hlp_GetInstanceID(other));
+		Npc_ClearAIQueue(self);
+		AI_Attack(self);
+	};
+};
 func void B_MM_ReactToCombatDamage()
 {
+	PrintDebugNpc(PD_MST_FRAME,"B_MM_ReactToCombatDamage");
+	SC_OneShot(other,self);
+	SC_RescaleDamage();
 	SaveReceivedDamage_Monster();
 	SetVisualType(self);
-	PrintDebugNpc(PD_MST_FRAME,"B_MM_ReactToCombatDamage");
 	self.aivar[AIV_PLUNDERED] = PRIO_ATTACKER;
 	if(C_PreyToPredator(self,other))
 	{
 		Npc_SetTarget(self,other);
 		Npc_ClearAIQueue(self);
 		AI_StartState(self,ZS_MM_Flee,0,"");
+	}
+	else
+	{
+		B_Npc_ChoiceTarget(self);
 	};
 	if(Josef_IsOrcDog(self))
 	{
@@ -823,16 +435,38 @@ func void ZS_MM_Attack()
 	AI_Standup(self);
 	AI_SetWalkMode(self,NPC_RUN);
 	Npc_GetTarget(self);
+//	PrintSS("2222222",other.name[0]);
 	Npc_SendPassivePerc(self,PERC_ASSESSWARN,other,self);
 };
 
 func int ZS_MM_Attack_Loop()
 {
+	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Attack_Loop");
+//	PrintSS("2222222",other.name[0]);
+	B_SpecStatesHandler(self);
+	if(self.aivar[AIV_IMPORTANT] == ID_FIREGOLEM)
+	{
+		if(getTimestamp() % 2)
+		{
+			if(!bFireGolem_Part_Spawned)
+			{
+				bFireGolem_Part_Spawned = true;
+//				Wld_InsertNpc(FireGolem_Part,"");
+				Print("2222");
+			};
+		}
+		else
+		{
+			bFireGolem_Part_Spawned = false;
+			Print("3333");
+		};
+	};
+//	PrintSS("3333333333",other.name[0]);
 	if(Npc_GetPermAttitude(self,other) != ATT_HOSTILE)
 	{
 		return LOOP_END;
 	};
-	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Attack_Loop");
+//	PrintSS("4444444444",other.name[0]);
 	if(other.attribute[ATR_HITPOINTS] <= 0)
 	{
 		B_MM_AssessBody();
@@ -844,7 +478,7 @@ func int ZS_MM_Attack_Loop()
 		PrintDebugNpc(PD_MST_LOOP,"...Ziel vorhanden!");
 		if(C_BodyStateContains(other,BS_RUN) || C_BodyStateContains(other,BS_JUMP))
 		{
-			PrintDebugNpc(PD_MST_LOOP,"...Ziel lВuft oder springt!");
+			PrintDebugNpc(PD_MST_LOOP,"...Ziel lР вЂ™РїС—Р…uft oder springt!");
 			if(Npc_GetStateTime(self) > self.aivar[AIV_BEGGAR])
 			{
 				PrintDebugNpc(PD_MST_CHECK,"...Ziel schon zu lange verfolgt!");
@@ -870,7 +504,7 @@ func int ZS_MM_Attack_Loop()
 	}
 	else
 	{
-		PrintDebugNpc(PD_ZS_Check,"...Ziel ist ungВltig oder kampf-unfВhig!");
+		PrintDebugNpc(PD_ZS_Check,"...Ziel ist ungР вЂ™РїС—Р…ltig oder kampf-unfР вЂ™РїС—Р…hig!");
 		if(self.aivar[AIV_FINDABLE] == HUNTER)
 		{
 			Npc_ClearAIQueue(self);
@@ -962,9 +596,24 @@ func void B_MM_AssessWarn()
 		};
 	};
 };
-
+func void B_MM_Respawn()
+{
+	AI_StartState(self,ZS_RespawnWait,0,"");
+};
 func void ZS_MM_AllScheduler()
 {
+	if(Npc_GetDeadTime(self))
+	{
+		var int lastTargetID; lastTargetID = B_Npc_GetLastTargetID(self);
+		var C_NPC asdqwe; asdqwe = Hlp_GetNpc(lastTargetID);
+		Npc_SetTarget(self,asdqwe);
+		Npc_GetTarget(self);
+//		PrintSSS(other.name[0],"ZS_MM_AllScheduler",asdqwe.name[0]);
+//		PrintSSS(self.name[0],"ZS_MM_AllScheduler",IntToString(B_Npc_GetLastTargetID(self)));
+		self.aivar[AIV_MM_DEAD_Timestamp] = 0;
+		AI_StartState(self,ZS_MM_Attack,0,"");
+		return;
+	};
 	PrintDebugNpc(PD_MST_FRAME,"ZS_MM_AllScheduler");
 //		Print("ZS_MM_AllScheduler22222222222222222");
 	if(Wld_IsTime(self.aivar[AIV_WARNTARGET],0,self.aivar[AIV_LASTHITBYRANGEDWEAPON],0) || (self.aivar[AIV_WARNTARGET] == OnlyRoutine))
@@ -1014,6 +663,7 @@ func void ZS_MM_Rtn_Default()
 func void ZS_MM_Rtn_Default_loop()
 {
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_Default_loop");
+	B_SpecStatesHandler(self);
 };
 
 func void ZS_MM_Rtn_Default_end()
@@ -1076,6 +726,7 @@ func void ZS_MM_Rtn_Sleep()
 func void ZS_MM_Rtn_Sleep_loop()
 {
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_Sleep_loop");
+	B_SpecStatesHandler(self);
 	if(!Wld_IsTime(self.aivar[AIV_WARNTARGET],0,self.aivar[AIV_LASTHITBYRANGEDWEAPON],0) && (self.aivar[AIV_WARNTARGET] != OnlyRoutine))
 	{
 		AI_StartState(self,ZS_MM_AllScheduler,1,"");
@@ -1109,6 +760,7 @@ func int ZS_MM_Rtn_Roam_loop()
 {
 	var int randomMove;
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_Roam_loop");
+	B_SpecStatesHandler(self);
 	if(!Wld_IsTime(self.aivar[AIV_Trigger3],0,self.aivar[AIV_GUARDMEMORY],0) && (self.aivar[AIV_Trigger3] != OnlyRoutine))
 	{
 		AI_StartState(self,ZS_MM_AllScheduler,1,"");
@@ -1176,6 +828,7 @@ func int ZS_MM_Rtn_Rest_Loop()
 {
 	var int randomMove;
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_Rest_Loop");
+	B_SpecStatesHandler(self);
 	if(!Wld_IsTime(self.aivar[AIV_HASBEENDEFEATEDINPORTALROOM],0,self.aivar[AIV_HangAroundStatus],0) && (self.aivar[AIV_HASBEENDEFEATEDINPORTALROOM] != OnlyRoutine))
 	{
 		AI_StartState(self,ZS_MM_AllScheduler,1,"");
@@ -1238,6 +891,7 @@ func void ZS_MM_Rtn_EatGround()
 func int ZS_MM_Rtn_EatGround_Loop()
 {
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_EatGround_Loop");
+	B_SpecStatesHandler(self);
 	if(!Wld_IsTime(self.aivar[AIV_GUARDITERATOR],0,self.aivar[AIV_FIGHTSPEACHFLAG],0) && (self.aivar[AIV_GUARDITERATOR] != OnlyRoutine))
 	{
 		AI_StartState(self,ZS_MM_AllScheduler,1,"");
@@ -1256,12 +910,12 @@ func void ZS_MM_Rtn_Wusel()
 {
 	PrintDebugNpc(PD_MST_FRAME,"ZS_MM_Rtn_Wusel");
 //	Npc_PercEnable(self,PERC_ASSESSTALK,B_AssessTalk);
-//	Npc_PercEnable(self,PERC_ASSESSDAMAGE,B_MM_ReactToDamage);
-//	Npc_PercEnable(self,PERC_ASSESSOTHERSDAMAGE,B_MM_ReactToOthersDamage);
-//	Npc_PercEnable(self,PERC_ASSESSMAGIC,B_AssessMagic);
+	Npc_PercEnable(self,PERC_ASSESSDAMAGE,B_MM_ReactToDamage);
+	Npc_PercEnable(self,PERC_ASSESSOTHERSDAMAGE,B_MM_ReactToOthersDamage);
+	Npc_PercEnable(self,PERC_ASSESSMAGIC,B_AssessMagic);
 	Npc_PercEnable(self,PERC_ASSESSENEMY,B_MM_AssessEnemy);
-//	Npc_PercEnable(self,PERC_ASSESSWARN,B_MM_AssessWarn);
-//	Npc_PercEnable(self,PERC_ASSESSBODY,B_MM_AssessBody);
+	Npc_PercEnable(self,PERC_ASSESSWARN,B_MM_AssessWarn);
+	Npc_PercEnable(self,PERC_ASSESSBODY,B_MM_AssessBody);
 	AI_SetWalkMode(self,NPC_RUN);
 	if(Hlp_StrCmp(Npc_GetNearestWP(self),self.wp) == FALSE)
 	{
@@ -1273,6 +927,7 @@ func int ZS_MM_Rtn_Wusel_loop()
 {
 	var int randomMove;
 	PrintDebugNpc(PD_MST_LOOP,"ZS_MM_Rtn_Wusel_loop");
+	B_SpecStatesHandler(self);
 	if(
 		!Wld_IsTime(self.aivar[AIV_MM_WuselStart],0,self.aivar[AIV_MM_WuselEnd],0) &&
 		(self.aivar[AIV_MM_WuselStart] != OnlyRoutine)
